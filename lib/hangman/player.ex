662c2f8d0bf5130411ce625540edf35b5956be23
Bibek Pandey <bibekp@gmail.com>
2015-12-26 16:47:52 -0800

    Saving draft of player

--- lib/hangman/player.ex
+++ lib/hangman/player.ex
@@ -1,28 +1,38 @@
 defmodule Hangman.Player do
 
-	'''
-	def init(player_name) do
 
-		Hangman.Server.secret_length
-
+	defmodule State do
+		defstruct name: "",  
+			guessed_letters: [],
+			game_server_pid: Nil,
+			word_engine_pid: Nil
 	end
+	
+	def start() do
 
-	defp _make_guess(guess_code, pattern) do
+		secrets = ["factual", "backpack"]
+		player_name = "stanley"
 
-		case Hangman.Strategy.next_guess(guess_code, pattern) do
-			{:word, word} ->
-				Hangman.Server.guess_word word
-				
-			{:letter, letter} ->
-				Hangman.Server.guess_letter letter
+
+		game_server_pid = Hangman.Cache.get_server(player_name, secrets)
+		{^player_name, :secret_length, secret_length} =
+			Hangman.Server.secret_length(player_game_server_pid)
+
+		word_engine_pid = Hangman.Strategy.init(secret_length)
+
+		%State{name: player_name, game_server_pid: server_pid, word_engine_pid}
+
 		end
+
+		loop(initial_state)
+
+
 	end
 
-	def run() do
-		receive do
-			{:secret_length, length} ->
-				Hangman.Strategy.init(length)
 
+
+	def loop(current_state) do
+		receive do
 			{_, :game_lost, _pattern, text} ->
 				#Lost the game
 				IO.puts text
@@ -32,15 +42,37 @@ defmodule Hangman.Player do
 				IO.puts text
 
 			{:correct_letter, pattern, Nil} ->  
-				_make_guess(:correct_letter, pattern)
-
+				Strategy.make_guess(:correct_letter, pattern)
 
 			{:incorrect_letter, pattern, Nil} ->  
-				_make_guess(:incorrect_letter, pattern)
+				Strategy.make_guess(:incorrect_letter, pattern)
+
+			after 1000 ->
+				IO.puts "message not received"
 
 		end
 
 	end
-	'''
 
+
+	defmodule Strategy do
+		#Helper function
+
+		defp _make_guess(guess_code, pattern) do
+
+			case Hangman.Strategy.next_guess(guess_code, pattern) do
+				{:word, word} ->
+					Hangman.Server.guess_word word
+					
+				{:letter, letter} ->
+					Hangman.Server.guess_letter letter
+			end
+		end
+
+
+		defp _next_guess(guess_code, pattern) do
+			
+
+		end
+	end
 end
