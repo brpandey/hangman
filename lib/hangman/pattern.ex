01158a7c5a1d991b6e03c3a99e5dfabade405183
Bibek Pandey <bibekp@gmail.com>
2016-01-17 23:37:32 -0800

    Prefixed private functions with do_ according to style guide

--- lib/hangman/pattern.ex
+++ lib/hangman/pattern.ex
@@ -1,23 +1,23 @@
 defmodule Hangman.Pattern do 
 
 	def update( pattern, secret, letter ) do
-		_update( String.codepoints(pattern), String.codepoints(secret), letter, [])
+		do_update( String.codepoints(pattern), String.codepoints(secret), letter, [])
 	end
 
 	#Base case, first is when secret codepoints list is finished
-	defp _update( _, [], _, value ), do: List.to_string(value)
+	defp do_update( _, [], _, value ), do: List.to_string(value)
 
 	#Base case, second is error condition, pattern can't be longer than secret
-	defp _update( [], _,  _, _ ), do: raise "pattern can't be longer than secret"
+	defp do_update( [], _,  _, _ ), do: raise "pattern can't be longer than secret"
 
 	#Match only if head of secret is the desired letter
-	defp _update( [ _ | p_tail ], [ letter | s_tail ], letter, value ) do 
-		_update( p_tail, s_tail, letter, [value | letter] )
+	defp do_update( [ _ | p_tail ], [ letter | s_tail ], letter, value ) do 
+		do_update( p_tail, s_tail, letter, [value | letter] )
 	end
 
 	#Clause indicating letter not in the secret head, so concat existing pattern character
-	defp _update( [ p_head | p_tail ], [ _ | s_tail ], letter, value ) do 
-		_update( p_tail, s_tail, letter, [value | p_head] )
+	defp do_update( [ p_head | p_tail ], [ _ | s_tail ], letter, value ) do 
+		do_update( p_tail, s_tail, letter, [value | p_head] )
 	end
 
 end
