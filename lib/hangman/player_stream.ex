f9352f26a015f659a7eeb95a83d91f3b9b8c8349
Bibek Pandey <bibekp@gmail.com>
2016-01-24 18:55:25 -0800

    Stream wrapped robot guessing game play :=)

---
+++ lib/hangman/player_stream.ex
@@ -0,0 +1,26 @@
+defmodule Hangman.Player.Stream do
+
+	alias Hangman.{Player.FSM}
+
+	def round(name, game_pid, notify_pid) do
+		Stream.resource(
+			fn -> 
+				{:ok, julio_pid} = FSM.start(name, :robot, game_pid, notify_pid)
+				julio_pid
+				end,
+
+			fn julio_pid ->
+				case FSM.r2d2_guess(julio_pid) do
+					{:game_reset, reply} -> {:halt, julio_pid}
+
+					# All other game states :game_keep_guessing ... :game_over
+					{_, reply} -> {[reply], julio_pid}							
+				end
+			end,
+			
+			fn julio_pid -> FSM.stop(julio_pid) end
+		)
+	end
+
+end
+
