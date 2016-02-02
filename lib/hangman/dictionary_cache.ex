eaed18f3717d4bb5c7238e4f4886c55b1e51f5ee
Bibek Pandey <bibekp@gmail.com>
2016-02-01 20:46:27 -0800

    Updated ets key retrieval method after StackOverflow question 35122608

--- lib/hangman/dictionary_cache.ex
+++ lib/hangman/dictionary_cache.ex
@@ -1,6 +1,8 @@
 defmodule Hangman.Dictionary.Cache do
 	#use GenServer
 
+	@ets_table_name :dictionary_cache
+
 	@dictionary_chunks 500
 	@words_chunk_size 1_000
 
@@ -15,37 +17,20 @@ defmodule Hangman.Dictionary.Cache do
 	#Total normal dictionary size is about @dictionary_chunks * @words_chunk_size = 200 * 1_000 = 200_000 words
 	#Total big dictionary size is about @dictionary_chunks * @words_chunk_size = 400 * 1_000 = 400_000 words
 
-	# This is for the hangman word dictionary (both normal and big)
-	# Specifically not dynamically generating atoms from lengths to serve as an assert 
-	# and restrict the number of atoms to this static map
 
-	# @chunk_key_map indexed by word length,
-	# returns atom key for the word chunks for that length
-	@chunk_key_map %{
-		2 => :chunk_key_2, 3 => :chunk_key_3, 4 => :chunk_key_4, 
-		5 => :chunk_key_5, 6 => :chunk_key_6, 7 => :chunk_key_7, 
-		8 => :chunk_key_8, 9 => :chunk_key_9, 10 => :chunk_key_10, 
-		11 => :chunk_key_11, 12 => :chunk_key_12, 13 => :chunk_key_13, 
-		14 => :chunk_key_14, 15 => :chunk_key_15, 16 => :chunk_key_16,
-		17 => :chunk_key_17, 18 => :chunk_key_18, 19 => :chunk_key_19, 
-		20 => :chunk_key_20, 21 => :chunk_key_21, 22 => :chunk_key_22, 
-		23 => :chunk_key_23, 24 => :chunk_key_24, 25 => :chunk_key_25, 
-		26 => :chunk_key_26, 27 => :chunk_key_27, 28 => :chunk_key_28
-	}
+	# Used to insert the word list chunks and frequency counter tallies, 
+	# indexed by word length 2..28, for both the normal and big dictionary file sizes
+	@possible_keys MapSet.new(2..28)
 
-	# @counter_key_map indexed by word length
-	# returns atom key of the counter object for that length
-	@counter_key_map %{
-		2 => :counter_key_2, 3 => :counter_key_3, 4 => :counter_key_4, 
-		5 => :counter_key_5, 6 => :counter_key_6, 7 => :counter_key_7, 
-		8 => :counter_key_8, 9 => :counter_key_9, 10 => :counter_key_10, 
-		11 => :counter_key_11, 12 => :counter_key_12, 13 => :counter_key_13, 
-		14 => :counter_key_14, 15 => :counter_key_15, 16 => :counter_key_16,
-		17 => :counter_key_17, 18 => :counter_key_18, 19 => :counter_key_19, 
-		20 => :counter_key_20, 21 => :counter_key_21, 22 => :counter_key_22, 
-		23 => :counter_key_23, 24 => :counter_key_24, 25 => :counter_key_25, 
-		26 => :counter_key_26, 27 => :counter_key_27, 28 => :counter_key_28
-	}
+	def get_ets_chunk_key(word_length) do
+		true = MapSet.member?(@possible_keys, word_length)
+		{:chunk, word_length}
+	end
+
+	def get_ets_counter_key(word_length) do
+		true = MapSet.member?(@possible_keys, word_length)
+		{:counter, word_length}
+	end	
 
 	# TODO : Split into two modules, Hangman.Dictionary.Cache and Hangman.Words.Stream
 
@@ -75,9 +60,14 @@ defmodule Hangman.Dictionary.Cache do
 	end
 
 
-	# Load dictionary word file into ets table :dictionary_cache
+	# Load dictionary word file into ets table @ets_table_name
+	# Segments of the dictionary are broken up into chunks 
+	# and stored in the ets
+	
+	# Letter frequency counters of the dictionary words 
+	# arranged by length are also stored in the ets
 	def load() do
-		:ets.new(:dictionary_cache, [:bag, :named_table, :protected])
+		:ets.new(@ets_table_name, [:bag, :named_table, :protected])
 
 		ws = Hangman.Words.Stream.new(:sorted_dictionary_stream, @dictionary_sorted_path)
 
@@ -98,8 +88,8 @@ defmodule Hangman.Dictionary.Cache do
 
 		fn_ets_insert_chunks = fn 
 			{words_chunk_list, length} -> 
-					key = Map.get(@chunk_key_map, length)
-					:ets.insert(:dictionary_cache, {key, words_chunk_list}) 
+					key = get_ets_chunk_key(length)
+					:ets.insert(@ets_table_name, {key, words_chunk_list}) 
 		end
 
 		Hangman.Words.Stream.words(ws)
@@ -109,26 +99,29 @@ defmodule Hangman.Dictionary.Cache do
 			|> Stream.each(fn_ets_insert_chunks)
 			|> Enum.take(@dictionary_chunks)
 
-		#IO.puts "ets info is: #{inspect :ets.info(:dictionary_cache)}\n"
+		IO.puts "ets info is: #{inspect :ets.info(@ets_table_name)}\n"
 
 		Hangman.Words.Stream.delete(ws)
 
 
 		fn_ets_insert_counters = fn 
 		 	{length, %Hangman.Counter{} = counter} ->  
-		 		key = Map.get(@counter_key_map, length)
-		 		:ets.insert(:dictionary_cache, {key, counter})
-		 		#IO.puts "Just inserted counter: #{inspect counter} for key: #{key}"
+		 		key = get_ets_counter_key(length)
+		 		:ets.insert(@ets_table_name, {key, counter})
+		 		IO.puts "Just inserted counter: #{inspect counter} for key: #{inspect key}"
 		end
 
+		key_sort_lambda = fn ({_, x}), ({_, y})  -> x <= y end
+
 		# Given all the keys we inserted, create the tallys and insert it into the ets
-		get_inserted_keys() |> Enum.map(&generate_tally(&1)) |> Enum.map(fn_ets_insert_counters)
+		get_inserted_ets_keys(@ets_table_name) 
+		|> Enum.sort(key_sort_lambda)
+		|> Enum.map(&generate_tally(@ets_table_name, &1)) 
+		|> Enum.map(fn_ets_insert_counters)
 
-
-		#IO.puts "ets info 2 is: #{inspect :ets.info(:dictionary_cache)}\n"
+		IO.puts "ets info 2 is: #{inspect :ets.info(@ets_table_name)}\n"
 
 	end	
-
 
 
 	# Retrieve tally counter
@@ -136,12 +129,12 @@ defmodule Hangman.Dictionary.Cache do
 	def lookup_tally(secret_length) 
 	when is_number(secret_length) and secret_length > 0 do
 
-		case Map.has_key?(@counter_key_map, secret_length) do
+		case MapSet.member?(@possible_keys, secret_length) do
 			true -> 
-				key = Map.get(@counter_key_map, secret_length)
+				key = get_ets_counter_key(secret_length)
 				
-				case :ets.lookup(:dictionary_cache, key) do
-					[] -> raise "Counter not found for key: #{key}"
+				case :ets.lookup(@ets_table_name, key) do
+					[] -> raise "Counter not found for key: #{inspect key}"
 					[{_key, counter}] -> counter
 				end
 
@@ -150,26 +143,12 @@ defmodule Hangman.Dictionary.Cache do
 
 	end
 
-	# Get inserted key lengths
-
-	defp get_inserted_keys() do
-		list = Enum.reduce(Map.keys(@chunk_key_map), [], fn head, acc -> 
-				case :ets.lookup(:dictionary_cache, Map.get(@chunk_key_map, head)) do
-					[] -> acc
-					_ -> [acc, head]
-				end
-			end)
-
-		List.flatten(list)
-	end
 
 	# Tally letter frequencies from all words of similiar length
 	# as specified by secret_length
 
-	defp generate_tally(secret_length) 
-	when is_number(secret_length) and secret_length > 0 do
-
-		key = Map.get(@chunk_key_map, secret_length)
+	defp generate_tally(table_name, key) 
+	when is_tuple(key) and tuple_size(key) == 2 do
 		
 		fn_reduce_words_into_counter = fn 
 			head, acc -> Hangman.Counter.add_unique_letters(acc, head)
@@ -181,10 +160,39 @@ defmodule Hangman.Dictionary.Cache do
 		end
 
 		counter = :ets.foldl(fn_reduce_key_chunks_into_counter, 
-			Hangman.Counter.new(), :dictionary_cache)
+			Hangman.Counter.new(), table_name)
 
-		{secret_length, counter}
+		{_, length} = key
+
+		{length, counter}
 	end
 
+	# Get inserted key lengths
 
+	defp get_inserted_ets_keys(table_name) when is_atom(table_name) do
+		eot = :"$end_of_table"
+
+		Stream.resource(
+			fn -> [] end,
+
+			fn acc ->
+				case acc do
+					[] -> 
+						case :ets.first(table_name) do
+							^eot -> {:halt, acc}
+							first_key -> {[first_key], first_key}						
+						end
+
+					acc -> 
+						case :ets.next(table_name, acc) do	
+							^eot -> {:halt, acc}
+							next_key ->	{[next_key], next_key}
+						end
+				end
+			end,
+
+			fn _acc -> :ok end
+		)
+	end
 end
+
