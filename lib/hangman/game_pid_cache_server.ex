16faf0233dc7c3c7873fb9fb95dae664db170b56
Bibek Pandey <bibekp@gmail.com>
2016-03-02 15:19:46 -0800

    Added typespecs and docs

--- lib/hangman/game_pid_cache_server.ex
+++ lib/hangman/game_pid_cache_server.ex
@@ -1,10 +1,21 @@
 defmodule Hangman.Game.Pid.Cache.Server do 
 	use GenServer
 
+  @moduledoc """
+  Module provides game server pid caching so
+  a game server doesn't need to be reloaded
+  on every use
+  """
+
   require Logger
 
 	@name __MODULE__
 
+  @doc """
+  GenServer start_link wrapper function
+  """
+
+  @spec start_link :: {:ok, pid}
 	def start_link do
 		Logger.info "Starting Hangman Game Pid Cache Server"
 
@@ -14,7 +25,12 @@ defmodule Hangman.Game.Pid.Cache.Server do
 		GenServer.start_link(@name, args, options)
 	end
 
+  @doc """
+  Checks registry cache for game server pid, returns cached pid or
+  if not found returns new game pid
+  """
 
+  @spec get_server_pid(String.t, String.t) :: pid
 	def get_server_pid(player_name, secret) do
 		
 		case Hangman.Game.Server.whereis(player_name) do
@@ -28,8 +44,18 @@ defmodule Hangman.Game.Pid.Cache.Server do
 		end
 	end
 
+  @doc """
+  GenServer callback to initialize server process
+  """
+  @callback init(term) :: {}
 	def init(_), do:	{:ok, nil}
 
+
+  @doc """
+  GenServer callback to retrieve game server pid
+  """
+  
+  @callback handle_call({:atom, String.t, String.t}, {}, term) :: {}
 	def handle_call({:get_server, player_name, secret}, _from, state) do
 
 		#Check the registry again for the pid -- safeguard against race condition
