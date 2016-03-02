3d67a04fa5ecee30bce064f9aa532ff78b87bef0
Bibek Pandey <bibekp@gmail.com>
2016-03-02 15:19:00 -0800

    Added typespecs and docs

--- lib/hangman/echo_fsm.ex
+++ lib/hangman/echo_fsm.ex
@@ -1,21 +1,57 @@
 defmodule Hangman.Player.Async.Echo do
 	@behaviour :gen_fsm
 
+  @moduledoc """
+  Module serves as an asynchronous echo server for
+  the asynchronous robot fsm player type
+
+  Allows the asynchronous robot fsm player to run a game 
+  asynchronously without having to be wrapped in a 
+  Stream.run, runs automatically itself
+  """
+
 	# External API
-  def start_link() do
+
+  @doc """
+  gen fsm start_link wrapper function
+  """
+  
+  @spec start_link :: {:ok, pid}
+  def start_link do
     :gen_fsm.start_link(__MODULE__, [], [])
   end
 
 
   # FSM API
+
+  @doc """
+  public api method to send an echo in the form of a async guess
+  call back to the Player.FSM
+  """
+
+  @spec echo_guess(pid, pid) :: {}
   def echo_guess(fsm_pid, other_pid) do
  	  :gen_fsm.send_event(fsm_pid, {:echo_guess, other_pid})
   end
 
   # FSM Callbacks
+
+  @doc """
+  gen fsm callback to initalize server process
+  """
+
+  @callback init(Keyword.t) :: {:atom, :atom, []}
   def init(_), do: { :ok, :echo, [] }
 
+
   # Asynchronous FSM Callbacks
+
+  @doc """
+  gen fsm callback for initial and only state echo
+  handles :echo_guess event
+  """
+
+  @callback echo({:atom, pid}, []) :: {:atom, :atom, []}
   def echo({:echo_guess, other_pid}, state) do
   	Hangman.Player.FSM.async_guess(other_pid)
   	{:next_state, :echo, state}
