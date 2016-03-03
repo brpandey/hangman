9f69384406af1fdcf903b8ab77c7f480849e37ef
Bibek Pandey <bibekp@gmail.com>
2016-03-02 16:31:36 -0800

    Minor formatting changes

--- lib/hangman/game_pid_cache_server.ex
+++ lib/hangman/game_pid_cache_server.ex
@@ -1,27 +1,28 @@
 defmodule Hangman.Game.Pid.Cache.Server do 
 	use GenServer
-
+  
   @moduledoc """
   Module provides game server pid caching so
   a game server doesn't need to be reloaded
   on every use
   """
-
+  
   require Logger
-
+  
 	@name __MODULE__
-
+  
   @doc """
   GenServer start_link wrapper function
   """
-
+  
   @spec start_link :: {:ok, pid}
+  
 	def start_link do
 		Logger.info "Starting Hangman Game Pid Cache Server"
-
+    
 		args = nil
 		options = [name: @name]
-
+    
 		GenServer.start_link(@name, args, options)
 	end
 
@@ -29,49 +30,48 @@ defmodule Hangman.Game.Pid.Cache.Server do
   Checks registry cache for game server pid, returns cached pid or
   if not found returns new game pid
   """
-
+  
   @spec get_server_pid(String.t, String.t) :: pid
+
 	def get_server_pid(player_name, secret) do
 		
 		case Hangman.Game.Server.whereis(player_name) do
-
 			:undefined ->
 				GenServer.call(@name, {:get_server, player_name, secret})
-
 			pid -> 
 				Hangman.Game.Server.load_game(pid, secret)
 				pid
 		end
 	end
-
+  
   @doc """
   GenServer callback to initialize server process
   """
   @callback init(term) :: {}
 	def init(_), do:	{:ok, nil}
-
-
+  
+  
   @doc """
   GenServer callback to retrieve game server pid
   """
   
   @callback handle_call({:atom, String.t, String.t}, {}, term) :: {}
-	def handle_call({:get_server, player_name, secret}, _from, state) do
 
+	def handle_call({:get_server, player_name, secret}, _from, state) do
+    
 		#Check the registry again for the pid -- safeguard against race condition
 		pid = 
-		case Hangman.Game.Server.whereis(player_name) do
-
-			:undefined -> 
-				{:ok, pid} = Hangman.Game.Server.Supervisor.start_child(player_name, secret)
-				pid
-
-			pid -> 
-				Hangman.Game.Server.load_game(pid, secret)
-				pid
-		end
-
+		  case Hangman.Game.Server.whereis(player_name) do
+			  :undefined -> 
+				  {:ok, pid} = 
+            Hangman.Game.Server.Supervisor.start_child(player_name, secret)
+				  pid
+			  pid ->
+          Hangman.Game.Server.load_game(pid, secret)
+				  pid
+		  end
+    
 		{:reply, pid, state}
 	end
-
+  
 end
