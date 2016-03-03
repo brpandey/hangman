b620a8a990c0740a21ee660fbba27a5498cbff81
Bibek Pandey <bibekp@gmail.com>
2016-03-02 17:32:26 -0800

    Added typespecs and docs

--- lib/hangman/pass_server.ex
+++ lib/hangman/pass_server.ex
@@ -1,27 +1,35 @@
 defmodule Hangman.Pass.Server do
   use GenServer
 
-  require Logger
-
-  alias Hangman.{Types.Reduction.Pass, Word.Chunks, Counter, Reduction.Engine}
-  alias Hangman.Dictionary.Cache, as: DictCache
-
 	@moduledoc """
-	Table owning process for hangman words pass state
-  for a given player, round number.
-
-	Uses ets table. This module primarily does unserialized reads
+	Module implements ets table owning process for hangman words 
+  pass state for a given player, round number.
+  
+  Performs primarily unserialized reads
 
 	pass_key :: {id, game_no, round_no}
-	ets_engine_key :: {:words, pass_key} | {:counter, pass_key}
-
+  reduce_key :: [{guess_letter, letter}, {regex_match_key, key}] |
+                  [{:game_start, bool}, {secret_length, pos_integer}]
 	"""
+
+  require Logger
+  
+  alias Hangman.{Types.Reduction.Pass, Word.Chunks, 
+                 Counter, Reduction.Engine}
+  
+  alias Hangman.Dictionary.Cache, as: DictCache
+  
   @name __MODULE__
 	@ets_table_name :engine_pass_table
 
 
   # External API
 
+  @doc """
+  GenServer start_link wrapper function
+  """
+  
+  @spec start_link :: Supervisor.on_start
   def start_link() do
     Logger.info "Starting Hangman Pass Server"
     args = {}
@@ -29,29 +37,56 @@ defmodule Hangman.Pass.Server do
     GenServer.start_link(@name, args, options)
   end
 
+  @doc """
+  Routine to stop server normally
+  """
+
+  @spec stop(pid) :: {}
 	def stop(pid) do
 		GenServer.call pid, :stop
 	end
 
+  @doc """
+  GenServer callback to initialize server process
+  """
+
+  @callback init(term) :: {}
   def init(_) do
     setup()
     {:ok, {}}
   end
 
+  @doc """
+  GenServer callback to retrieve game server pid
+  """
+  
+  @callback handle_call(:atom, {}, {}) :: {}
 	def handle_call(:stop, _from, {}) do
 		{ :stop, :normal, :ok, {}}
 	end 
 
+  @doc """
+  GenServer callback to cleanup server state
+  """
+
+  @callback terminate(reason :: term, {}) :: term | no_return
 	def terminate(_reason, _state) do
 		:ok
 	end
 
+  # Loads ets table type set
+  
+  @spec setup :: :atom
 	defp setup() do
 		:ets.new(@ets_table_name, [:set, :named_table, :public])
 	end
 
-  # request not serialized through server process, since we are doing reads
-	# game start initial pass routine
+  @doc """
+  Routine retrieves pass tally given game start pass key
+  Request not serialized through server process, since we are doing reads
+  """
+
+  @spec get_pass(atom, tuple, Keyword.t) :: tuple
 	def get_pass(:game_start, {id, game_no, round_no} = pass_key, reduce_key)
 	when is_binary(id) and is_number(game_no) and is_number(round_no) do
 
@@ -71,37 +106,54 @@ defmodule Hangman.Pass.Server do
 		pass_info = %Pass{ size: pass_size, tally: tally, last_word: ""}
 
 		# Store pass info into ets table for round 2 (next pass)
+    # Allow writer engine to execute (and distribute) as necessary
     Hangman.Pass.Writer.write(pass_key, chunks)
 	
 		{pass_key, pass_info}
 	end
 
-  # game keep guessing engine pass routine
+
+  @doc """
+  Game keep guessing engine pass routine
+  Routine retrieves pass tally given pass key
+  Request not serialized through server process, since we are doing reads
+  """
+
+  @spec get_pass(atom, tuple, Keyword.t) :: tuple
 	def get_pass(:game_keep_guessing, {id, game_no, round_no} = pass_key, 
                reduce_key)
  	when is_binary(id) and is_number(game_no) and is_number(round_no) do
     
 		{:ok, exclusion_set} = Keyword.fetch(reduce_key, :guessed_letters)
 		{:ok, regex_key} = Keyword.fetch(reduce_key, :regex_match_key)
-    
+  
+    # Send pass and reduce information off to Engine server
+    # to execute (and distribute) as appropriate
     pass_info = Engine.reduce(pass_key, regex_key, exclusion_set)
 
 		{pass_key, pass_info}
 	end
 
 
-	# "get pass chunks from ets table with pass key"
+	@doc """
+  Retrieves pass chunks from ets table with pass key"
+  """
+  
+  @spec read_chunks(tuple) :: Chunks.t | no_return
 	def read_chunks({id, game_no, round_no} = pass_key)
 	when is_binary(id) and is_number(game_no) and is_number(round_no) do
 		
 		# Using match instead of lookup, to keep processing on the ets side
 		case :ets.match_object(@ets_table_name, {pass_key, :_}) do
-			[] -> raise Hangman.Error, "counter not found for key: #{inspect pass_key}"
+			[] -> 
+        raise Hangman.Error, 
+        "counter not found for key: #{inspect pass_key}"
 
 			[{_key, chunks}] ->
 				%Chunks{} = chunks # quick assert
 
-				# delete this current pass in the table, since we only keep 1 pass for each user
+				# delete this current pass in the table, 
+        # since we only keep 1 pass for each user
 				:ets.match_delete(@ets_table_name, {pass_key, :_})
     
 				# return chunks :)
