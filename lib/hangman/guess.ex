435d1def186a0d76d982364119443b493fb53b46
Bibek Pandey <bibekp@gmail.com>
2016-03-06 20:24:06 -0800

    fleshed out the guess types even more

--- lib/hangman/guess.ex
+++ lib/hangman/guess.ex
@@ -1,9 +1,22 @@
 defmodule Hangman.Guess do
   @moduledoc """
-  Module implements guess abstraction types only
+  Module primarily implements guess types
   """
 
-  @type t :: {:guess_letter, String.t} | {:guess_word, String.t}
+  @type t :: {:guess_letter, String.t} | {:guess_word, String.t} 
+
+
+  @typedoc """
+  Directive doesn't actually contain the guess, just direction
+  """
+  
+  @type directive :: :guess_last_word | :robot_guess | :choose_letters
+
+  @type option :: {:game_choose_letter, String.t} | {:game_last_word, String.t}
+
+  @typedoc """
+  Used by round abstraction to understand previous guess results
+  """
 
   @type context ::  ({:game_start, secret_length :: pos_integer}) |
   ({:game_keep_guessing, :correct_letter, letter :: String.t, 
