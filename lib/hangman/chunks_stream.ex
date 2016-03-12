2695742a72965fb98f79a48a9da406cb9d85d110
Bibek Pandey <bibekp@gmail.com>
2016-03-12 12:45:53 -0800

    Updated tab spaces

--- lib/hangman/chunks_stream.ex
+++ lib/hangman/chunks_stream.ex
@@ -10,8 +10,8 @@ defmodule Chunks.Stream do
   @grouped Dictionary.grouped
   @chunked Dictionary.chunked
 
-	# A chunk contains at most 2_000 words
-	@chunk_words_size 2_000
+  # A chunk contains at most 2_000 words
+  @chunk_words_size 2_000
 
   @doc """
   Transforms `grouped` word `stream` of the form 
@@ -26,41 +26,41 @@ defmodule Chunks.Stream do
   @spec transform(Enumerable.t, :atom, :atom) :: Enumerable.t
   def transform(stream, @grouped, @chunked) do
 
-	  # lambda to split stream into chunks based on generated chunk id
-		# Uses 1 + div() function to group consecutive, sorted words
-		# Takes into account the current word-length-group index position and 
-		# specified words-chunk buffer size, to determine chunk id
+    # lambda to split stream into chunks based on generated chunk id
+    # Uses 1 + div() function to group consecutive, sorted words
+    # Takes into account the current word-length-group index position and 
+    # specified words-chunk buffer size, to determine chunk id
 
-		#	A) Example of word stream before chunking
-		#	{6, "mugful", 8509}
-		#	{6, "muggar", 8510}
-		#	{6, "mugged", 8511}
-		#	{6, "muggee", 8512}
+    # A) Example of word stream before chunking
+    # {6, "mugful", 8509}
+    # {6, "muggar", 8510}
+    # {6, "mugged", 8511}
+    # {6, "muggee", 8512}
 
-		fn_split_into_chunks = fn 
-			{length_group, group_index, _} -> 
-				_chunk_id = length_group * ( 1 + div(group_index, @chunk_words_size))
-		end
+    fn_split_into_chunks = fn 
+      {length_group, group_index, _} -> 
+        _chunk_id = length_group * ( 1 + div(group_index, @chunk_words_size))
+    end
 
-		# lambda to normalize chunks
-		# Flatten out / normalize chunks so that they contain 
+    # lambda to normalize chunks
+    # Flatten out / normalize chunks so that they contain 
     # only a list of words, and word length size
 
-		# B) Example of chunk, before normalization
-		#	[{6, "mugful", 8509}, {6, "muggar", 8510}, {6, "mugged", 8511},
-		#	 {6, "muggee", ...}, {6, ...}, {...}, ...]
+    # B) Example of chunk, before normalization
+    # [{6, "mugful", 8509}, {6, "muggar", 8510}, {6, "mugged", 8511},
+    #  {6, "muggee", ...}, {6, ...}, {...}, ...]
 
     # Does a Enum.map_reduce, in that the length_key is the acc
     # and the word because the mapped value that is enumerated out
 
-		fn_normalize_chunks = fn 
-			chunk -> 
-				Enum.map_reduce(chunk, "", 
-					fn {length, _, word}, _acc -> {word, length} end)
-		end
+    fn_normalize_chunks = fn 
+      chunk -> 
+        Enum.map_reduce(chunk, "", 
+          fn {length, _, word}, _acc -> {word, length} end)
+    end
 
-		#	C) Example of chunk after normalization
-		#	{["mugful", "muggar", "mugged", "muggee", ...], 6}
+    # C) Example of chunk after normalization
+    # {["mugful", "muggar", "mugged", "muggee", ...], 6}
 
 
     stream 
