7baa0af9b24086b56641c83d5bb8608816292b07
Bibek Pandey <bibekp@gmail.com>
2016-03-13 21:43:46 -0700

    Added doctests, commented out debug_spawn, added error condition

--- lib/hangman/web.ex
+++ lib/hangman/web.ex
@@ -2,7 +2,29 @@ defmodule Web do
   use Plug.Router
 
   @moduledoc """
-  Implements `Hangman` http web server for playing `Hangman` games.
+  Module provides access to small and simple http web server for playing 
+  `Hangman` games via a tool such as curl or HTTPoison.
+  
+  ## Example
+
+      iex> HTTPoison.get("http://127.0.0.1:3737/play?name=julio&secret=kiwi")
+
+      {:ok,
+      %HTTPoison.Response{body: "(#) ----; score=1; status=KEEP_GUESSING (#) ----; score=2; status=KEEP_GUESSING (#) ----; score=3; status=KEEP_GUESSING (#) -I-I; score=4; status=KEEP_GUESSING (#) -I-I; score=5; status=KEEP_GUESSING (#) -I-I; score=6; status=KEEP_GUESSING (#) -I-I; score=25; status=GAME_LOST (#) Game Over! Average Score: 25.0, # Games: 1, Scores:  (KIWI: 25) ",
+      headers: [{"server", "Cowboy"}, {"date", "Mon, 14 Mar 2016 04:27:39 GMT"},
+      {"content-length", "345"},
+      {"cache-control", "max-age=0, private, must-revalidate"},
+      {"content-type", "text/plain; charset=utf-8"}], status_code: 200}}
+
+      iex> HTTPoison.get("http://127.0.0.1:3737/play?name=julio&random=2")
+
+      {:ok,
+      %HTTPoison.Response{body: "(#) --E-----------; score=1; status=KEEP_GUESSING (#) --E--------O--; score=2; status=KEEP_GUESSING (#) --E--------O--; score=3; status=KEEP_GUESSING (#) --E----C---O--; score=4; status=KEEP_GUESSING (#) PREVARICATIONS; score=4; status=GAME_WON (#) ---------; score=1; status=KEEP_GUESSING (#) --A------; score=2; status=KEEP_GUESSING (#) -IA----I-; score=3; status=KEEP_GUESSING (#) -IA----I-; score=4; status=KEEP_GUESSING (#) -IAG---I-; score=5; status=KEEP_GUESSING (#) DIAGNOSIS; score=5; status=GAME_WON (#) Game Over! Average Score: 4.5, # Games: 2, Scores:  (PREVARICATIONS: 4) (DIAGNOSIS: 5) ",
+      headers: [{"server", "Cowboy"}, {"date", "Mon, 14 Mar 2016 04:29:05 GMT"},
+      {"content-length", "601"},
+      {"cache-control", "max-age=0, private, must-revalidate"},
+      {"content-type", "text/plain; charset=utf-8"}], status_code: 200}}
+  
   """
 
   plug :match
@@ -13,7 +35,7 @@ defmodule Web do
     Plug.Adapters.Cowboy.http(__MODULE__, nil, port: 3737)
   end
 
-  # curl 'http://localhost:3737/play?name=julio&secret=woodpecker'
+
 
   @docp "Get macro, matches GET request and /play"
   get "/play" do
@@ -40,7 +62,7 @@ defmodule Web do
       secrets = [secrets]
     end
 
-    debug_spawn("secret.txt", secrets)
+    if secrets == nil, do: raise "Can't run hangman with no secrets"
 
     rounds = Player.Game.web_run(name, :robot, secrets, false, false)
     value = format_rounds(rounds)
@@ -48,13 +70,15 @@ defmodule Web do
     Plug.Conn.assign(conn, :response, value)
   end
 
+'''
   defp debug_spawn(file_name, term) do
     path = "./tmp"
     spawn(fn ->
-      "#{path}/#{file_name}"
+      "{path}/{file_name}"
       |> File.write!(term)
       end)
   end
+'''
 
   @spec format_rounds([String.t]) :: [String.t]
   defp format_rounds(rounds) do
