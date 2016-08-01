3df660a4367b5720744fa319c3492f9853feb850
Bibek Pandey <bibekp@gmail.com>
2016-08-01 14:09:15 -0700

    Eliminated an unnecessary line with a variable assignment in function head

--- lib/hangman/player.ex
+++ lib/hangman/player.ex
@@ -28,11 +28,9 @@ defmodule Hangman.Player do
 
   @name __MODULE__
 
-  defstart start_link({player_name, player_type, display, game_pid, event_pid}), 
+  defstart start_link(args = {player_name, player_type, display, game_pid, event_pid}), 
   when: is_binary(player_name) and is_atom(player_type) and is_boolean(display) 
-  and is_pid(game_pid) and is_pid(event_pid) do
-
-    args = {player_name, player_type, display, game_pid, event_pid}
+  and is_pid(game_pid) and is_pid(event_pid) and is_tuple(args) do
 
     Logger.info "Starting Hangman Player Server"
 
