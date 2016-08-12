2cbd66f5120a064fd48c6b406f00d885c1b20b30
Bibek Pandey <bibekp@gmail.com>
2016-08-11 19:48:59 -0700

    Updated player handler test wrt to alert placement

--- test/hangman/player_handler_test.exs
+++ test/hangman/player_handler_test.exs
@@ -4,22 +4,24 @@ defmodule Hangman.Player.Handler.Test do
   alias Hangman.{Player}
 
   setup_all do
-    IO.puts "Player Game Test"
+    IO.puts "Player Handler Test"
 
     :ok
   end
 
   test "test running 2 robot games and 2 human games" do 
 
-    secrets = ["asparagus", "voluptuous"]
-
-    {:ok, _apid} = Player.Alert.Supervisor.start_child("c3po_test", nil)
-    Player.Handler.run(:cli, "c3po_test", :robot, secrets, false, true)
-
     secrets = ["mitochondria", "eject"]
 
-    {:ok, _apid} = Player.Alert.Supervisor.start_child("jedi_test", nil)
     Player.Handler.run(:cli, "jedi_test", :human, secrets, true, false)
+
+
+    secrets = ["asparagus", "voluptuous"]
+
+    {:ok, apid} = Player.Alert.Supervisor.start_child("c3po_test", nil)
+    Player.Handler.run(:cli, "c3po_test", :robot, secrets, false, true)
+    Player.Alert.Handler.stop(apid)
+
   end
 
   @tag timeout: 90_000 # 90 secs
@@ -27,7 +29,9 @@ defmodule Hangman.Player.Handler.Test do
 
     secrets = ["porcupine", "eel"]
 
+    {:ok, apid} = Player.Alert.Supervisor.start_child("jedi_test", nil)
     Player.Handler.run(:cli, "photographer_test", :human, secrets, true, true)
+    Player.Alert.Handler.stop(apid)
   end
 
 end
