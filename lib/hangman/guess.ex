3831b973d0d8902751bafe50484b23bbe698fef3
Bibek Pandey <bibekp@gmail.com>
2016-08-29 17:01:28 -0700

    Minor format update

--- lib/hangman/guess.ex
+++ lib/hangman/guess.ex
@@ -1,7 +1,5 @@
 defmodule Hangman.Guess do
-  @moduledoc """
-  Module implements `Guess` types
-  """
+  @moduledoc "Module implements `Guess` types"
 
   @type t :: {:guess_letter, String.t} | {:guess_word, String.t}
 
