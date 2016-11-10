51a0fe19226c6f78553ea4731f48d70f76f06fe2
Bibek Pandey <bibekp@gmail.com>
2016-11-09 16:14:06 -0800

    Refactored unit test into describe blocks and separate setup methods

--- test/hangman/strategy_test.exs
+++ test/hangman/strategy_test.exs
@@ -4,8 +4,121 @@ defmodule Hangman.Letter.Strategy.Test do
   alias Hangman.{Letter.Strategy, Pass, Counter}
 
 
-  test "Strategy retrieval common" do
-     
+  describe "Strategy retrieval common for a large pass" do
+
+    setup :setup_large_pass
+
+    test "self-selecting auto mode", %{letter: letter, pass: pass} do
+      # Choose guess
+      strategy = Strategy.new |> Strategy.process(pass)
+      
+      assert {:guess_letter, ^letter} = Strategy.guess(strategy)
+      
+    end
+    
+    test "interactive choices mode", %{letter: letter, pass: pass} do
+      # Choose guess
+      strategy = Strategy.new |> Strategy.process(:choices, pass)
+      
+      str = "Player fozzie, Round 3, WAKA WAKA.\n5 weighted letter choices :" 
+      <> "  i:4843 a*:4368 s:4334 o:3920 n:3736 (* robot choice)"
+      
+      assert {:guess_letter, str} == 
+        Strategy.choices(strategy, %Hangman.Round{id: "fozzie", num: 3, status_text: "WAKA WAKA"})
+      
+      assert {:guess_letter, letter} == 
+        strategy |> Strategy.guess(:choices, {:guess_letter, letter})
+      
+    end
+    
+  end
+  
+
+  describe "Strategy retrieval common for a small pass" do
+
+    setup :setup_small_pass
+
+    test "self selecting auto mode", %{valid_letters: valid_letters, pass: pass} do
+
+      # Choose guess
+      strategy = Strategy.new |> Strategy.process(pass)
+      {:guess_letter, letter} = Strategy.guess(strategy)
+      
+      # Ensure the recommended letter guess is within the precalculated set
+      assert letter in valid_letters       
+    end
+
+    test "interactive choices mode", %{valid_letters: _valid_letters, pass: pass} do
+
+      strategy = Strategy.new |> Strategy.process(:choices, pass)
+
+      str = "Possible hangman words left, 4 words: [\"conductors\", "
+      <> "\"corruptors\", \"polygynous\", \"voluptuous\"]\n\nPlayer "
+      <> "piggy, Round 1, KERMIE.\n5 weighted letter choices :  u:4 p:3 t:3 c*:2 l:2 (* robot choice)"
+
+      assert {:guess_letter, str} == 
+        Strategy.choices(strategy, %Hangman.Round{id: "piggy", num: 1, status_text: "KERMIE"})      
+    end
+
+
+  end
+
+
+  describe "Strategy when last word reached" do
+
+    setup :setup_last_pass
+    
+    test "self selecting auto mode", %{pass: pass} do
+      token = "asparagus"
+      
+      # Choose guess
+      strategy = Strategy.new |> Strategy.process(pass)
+      
+      assert {:guess_word, ^token} = Strategy.guess(strategy)
+    end
+    
+    test "interactive choices mode", %{pass: pass} do
+      token = "asparagus"
+      
+      # Choose guess
+      strategy = Strategy.new |> Strategy.process(:choices, pass)
+      
+      assert {:guess_word, "asparagus",
+              "Player kermit, Round 7, KEEP GUESSING.\nLast word left: asparagus"} = 
+        strategy |> Strategy.choices(%Hangman.Round{id: "kermit", num: 7, status_text: "KEEP GUESSING"})
+    end
+    
+  end
+
+  
+  describe "Strategy when pass is empty" do
+
+    test "self selecting auto mode" do
+      
+      pass_info = %Pass{ size: 0, tally: nil, last_word: "quasar", possible: ""}
+      
+      # Process pass
+      
+      assert catch_error(Strategy.new |> Strategy.process(pass_info)) ==
+        %HangmanError{message: "Word not in dictionary"}
+    end
+
+    test "interactive choices mode" do
+      
+      pass_info = %Pass{ size: 0, tally: nil, last_word: "quasar", possible: ""}
+      
+      # Process pass
+      
+      assert catch_error(Strategy.new |> Strategy.process(:choices, pass_info)) ==  
+        %HangmanError{message: "Word not in dictionary"}
+    end
+
+  end
+  
+  
+
+  def setup_large_pass _context do
+    
     eng_letter_freq = %{
       "a" => 8.167, "b" => 1.492, "c" => 2.782, "d" => 4.253, "e" => 12.702, 
       "f" => 2.228, "g" => 2.015, "h" => 6.094, "i" => 6.966, "j" => 0.153,
@@ -15,15 +128,15 @@ defmodule Hangman.Letter.Strategy.Test do
       "z" => 0.074}
     
     tally = Counter.new(%{"a" => 4368, "b" => 1190, "c" => 2458, "d" => 1570, "f" => 722, "g" => 2184, "h" => 1574, "i" => 4843, "j" => 92, "k" => 747, "l" => 2952, "m" => 1771, "n" => 3736, "o" => 3920, "p" => 1528, "q" => 88, "r" => 3157, "s" => 4334, "t" => 3189, "u" => 2078, "v" => 361, "w" => 614, "x" => 100, "y" => 1313, "z" => 151})
-
+    
     tuple_list = Counter.items(tally)
-
+    
     weighted = 
       Enum.reduce(tuple_list, %{}, fn {k,v}, acc -> 
         weighted_v = Map.get(eng_letter_freq, k) * v
         Map.put(acc, k, weighted_v)
       end)
-
+    
     # Do a Schwartzian Transform
     # map the elements to a decorated value, sort the decorated value, 
     # and reflect that back in the new sorted order of the elements
@@ -34,34 +147,11 @@ defmodule Hangman.Letter.Strategy.Test do
     
     pass_info = %Pass{ last_word: "", size: 6697, tally: tally }
     
-
-    # AUTO mode
-
-    # Choose guess
-    strategy = Strategy.new |> Strategy.process(pass_info)
-
-    assert {:guess_letter, ^letter} = Strategy.guess(strategy)
-
-
-    # CHOICES mode
-
-    # Choose guess
-    strategy = Strategy.new |> Strategy.process(:choices, pass_info)
-
-    str = "Player fozzie, Round 3, WAKA WAKA.\n5 weighted letter choices :  i:4843 a*:4368 s:4334 o:3920 n:3736 (* robot choice)"
-    
-    assert {:guess_letter, ^str} = 
-      Strategy.choices(strategy, %Hangman.Round{id: "fozzie", num: 3, status_text: "WAKA WAKA"})
-
-    assert {:guess_letter, ^letter} = 
-      strategy 
-      |> Strategy.guess(:choices, {:guess_letter, letter})
-
+    [letter: letter, pass: pass_info]
   end
 
 
-
-  test "Strategy retrieval common small pass" do
+  def setup_small_pass _context do
 
     # word_set_size = %{micro: 2, tiny: 5, small: 9, large: 550}
 
@@ -92,34 +182,22 @@ defmodule Hangman.Letter.Strategy.Test do
     # so that we can easily grab the first element
     # E.g.  [{2, ["c", "l", "n", "r"]}, {1, ["d", "g", "v", "y"]}]
     # And then {2, ["c", "l", "n", "r"]}
-
-    {_, valid_letters} = 
-      result 
-      |> Enum.sort_by(&Kernel.elem(&1, 0), &>=/2) 
-      |> List.first
-
-    # Choose guess
-    strategy = Strategy.new |> Strategy.process(pass_info)
-    {:guess_letter, letter} = Strategy.guess(strategy)
-
-    # Ensure the recommended letter guess is within the precalculated set
-    assert letter in valid_letters   
-  end
-
-
-  test "Strategy when last word" do
     
-    token = "asparagus"
-      
-    tally = Counter.new(%{"p" => 1, "r" => 1, "s" => 1, "u" => 1})
-      
-    pass_info = %Pass{ size: 1, tally: tally, last_word: token, possible: ""}
-      
-    # Choose guess
-    strategy = Strategy.new |> Strategy.process(pass_info)
+    {_, valid_letters} = result 
+    |> Enum.sort_by(&Kernel.elem(&1, 0), &>=/2) 
+    |> List.first
 
-    assert {:guess_word, ^token} = Strategy.guess(strategy)
+    [valid_letters: valid_letters, pass: pass_info]
   end
 
+
+  def setup_last_pass _context do
+
+    token = "asparagus"
+    tally = Counter.new(%{"p" => 1, "r" => 1, "s" => 1, "u" => 1})    
+    pass_info = %Pass{ size: 1, tally: tally, last_word: token, possible: ""}
+
+    [pass: pass_info]
+  end
 
 end
